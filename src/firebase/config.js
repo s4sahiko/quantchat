97d2a343b513c600736c874bb0d8aa458d44f58e
s4sahiko <s4sahiko@gmail.com>
2026-03-28 09:41:44 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
-import firebaseConfig from '../../firebase-applet-config.json';
+import firebaseConfig from '@/firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
